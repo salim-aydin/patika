24250bac82711d2c03c8801cc05aff2c041ce591
salim-aydin <salimaydin340@gmail.com>
2023-05-08 00:53:16 +0300

    havaEtkinlik

---
+++ javaOdev/11-havaEtkinlik/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Mainn {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Hava sıcaklığını giriniz : ");
+
+        int sicaklik = sc.nextInt();
+
+        if (sicaklik < 5) {
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if (sicaklik >= 5 && sicaklik <= 15) {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        } else if (sicaklik >= 10 && sicaklik <= 25) {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        } else if (sicaklik >= 25) {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        } else {
+            System.out.println("Hatalı giriş yaptınız.");
+
+        }
+    }
+
+}
