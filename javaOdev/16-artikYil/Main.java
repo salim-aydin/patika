bb34e07698e86527f5d8936f09a6c1b69a9abe45
Salim Aydın <99368865+salim-aydin@users.noreply.github.com>
2023-05-13 01:14:32 +0300

    Create Main.java
    Bu kodda, artık yıl kontrolünü tek bir satırda gerçekleştirebiliriz. İlk olarak, yılın 4'e bölünebilmesi gerektiği için (year % 4 == 0) koşulunu kontrol ederiz. Sonra, yılın 100'e bölünememesi (year % 100 != 0) veya 400'e bölünebilmesi (year % 400 == 0) koşulunu sağlaması gerektiği için bu iki koşulu bir OR operatörü (||) ile birleştiririz. Bu iki ana koşul, AND operatörü (&&) ile birleştirilerek isLeapYear boolean değişkenine atanır. Bu şekilde kod daha sade ve okunabilir hale gelir.

---
+++ javaOdev/16-artikYil/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Yıl Giriniz: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+
+        if (isLeapYear) {
+            System.out.println(year + " bir artık yıldır!");
+        } else {
+            System.out.println(year + " bir artık yıldır değildir!");
+        }
+    }
+}
