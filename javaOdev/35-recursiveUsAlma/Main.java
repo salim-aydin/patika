103e99f23b4e793f482289c23b58858f3c4c5f61
Salim Aydın <99368865+salim-aydin@users.noreply.github.com>
2023-05-21 21:35:59 +0300

    Create Main.java

---
+++ javaOdev/35-recursiveUsAlma/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Taban değerini giriniz: ");
+        int taban = scanner.nextInt();
+
+        System.out.print("Üs değerini giriniz: ");
+        int us = scanner.nextInt();
+
+        int sonuc = usAlma(taban, us);
+        System.out.println("Sonuç: " + sonuc);
+    }
+
+    public static int usAlma(int taban, int us) {
+        if (us == 0) {
+            return 1;
+        } else {
+            return taban * usAlma(taban, us - 1);
+        }
+    }
+}
