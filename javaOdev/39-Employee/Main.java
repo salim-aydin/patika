d68d7360e1feb0575d7ff417ceec9d4e15fe362d
Salim Aydın <99368865+salim-aydin@users.noreply.github.com>
2023-05-21 22:08:58 +0300

    Create Main.java

---
+++ javaOdev/39-Employee/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Alex",2000,45,1985);
+        Employee e2 = new Employee("Michael",2000,50,2000);
+        e1.printInfo();
+        e2.printInfo();
+    }
+}
