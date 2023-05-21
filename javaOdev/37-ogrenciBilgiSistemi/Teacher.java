54de42b735b3dcb836c321860c7c7b82894e29b9
Salim Aydın <99368865+salim-aydin@users.noreply.github.com>
2023-05-21 22:05:03 +0300

    Create Teacher.java

---
+++ javaOdev/37-ogrenciBilgiSistemi/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+    String name;
+    String npno;
+    String branch;
+
+    public Teacher(String name, String npno, String branch){
+        this.name=name;
+        this.npno=npno;
+        this.branch=branch;
+    }
+}
